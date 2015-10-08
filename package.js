3d09b7219078d9fdc4c20291f470416ca6cbc920
Lauricio Su <me@lauricio.com>
2015-10-08 21:29:07 +0630

    wip

--- package.js
+++ package.js
@@ -11,12 +11,19 @@ var CS = [C, S];
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
+  // Core
+  api.use([
+    'ecmascript',
+    'session'
+    ]);
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@2.5.0_3'
+    'lauricio:less-autoprefixer@2.5.0_3',
+    'ox2:inject-style@1.0.0'
     ]);
   api.addFiles('lib/oo-color-variables.import.less', C);
   api.addFiles('lib/oo-colors.less', C);
+  api.addFiles('lib/oo-colors-theming.js', C);
 });
 
 Package.onTest(function(api) {
